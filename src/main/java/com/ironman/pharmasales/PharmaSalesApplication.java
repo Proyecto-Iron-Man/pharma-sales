13cf1e960633787302111f87236a8847015ef2c7
AbelPalominoRojas <abel.palominorojas@gmail.com>
2023-07-15 21:20:29 -0500

    Start Project

---
+++ src/main/java/com/ironman/pharmasales/PharmaSalesApplication.java
@@ -0,0 +1,13 @@
+package com.ironman.pharmasales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaSalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironman/pharmasales/PharmaSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironman.pharmasales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
