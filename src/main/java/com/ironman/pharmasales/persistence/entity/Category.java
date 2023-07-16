a5715803b2f497eec22e53152548c367481d2d60
AbelPalominoRojas <abel.palominorojas@gmail.com>
2023-07-15 22:15:37 -0500

    Add Lombok

--- src/main/java/com/ironman/pharmasales/persistence/entity/Category.java
+++ src/main/java/com/ironman/pharmasales/persistence/entity/Category.java
@@ -1,9 +1,13 @@
 package com.ironman.pharmasales.persistence.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "categories")
 @Entity
 public class Category {
@@ -24,75 +28,4 @@ public class Category {
 
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
-
-    // Constructors
-    public Category() {
-    }
-
-    public Category(Long id, String name, String description, String keyword, String state, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.keyword = keyword;
-        this.state = state;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
-
-    // Encapsulation
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getKeyword() {
-        return keyword;
-    }
-
-    public void setKeyword(String keyword) {
-        this.keyword = keyword;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 }
