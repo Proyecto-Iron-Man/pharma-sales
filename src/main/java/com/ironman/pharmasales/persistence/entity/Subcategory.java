b27f9d6d4c976768bf1c7736029da05c68e18051
AbelPalominoRojas <abel.palominorojas@gmail.com>
2023-07-29 08:53:59 -0500

    Subcategory: add persistence

---
+++ src/main/java/com/ironman/pharmasales/persistence/entity/Subcategory.java
@@ -0,0 +1,38 @@
+package com.ironman.pharmasales.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+// Lombok Start
+@Data
+// Lombok End
+
+// JPA Start
+@Entity
+@Table(name = "subcategories")
+// JPA End
+public class Subcategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    private String description;
+
+    private String keyword;
+
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    private String state;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/ironman/pharmasales/persistence/repository/SubcategoryRepository.java
@@ -0,0 +1,7 @@
+package com.ironman.pharmasales.persistence.repository;
+
+import com.ironman.pharmasales.persistence.entity.Subcategory;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SubcategoryRepository extends CrudRepository<Subcategory, Long> {
+}
