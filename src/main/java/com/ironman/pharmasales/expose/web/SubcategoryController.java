b1e74642e39ec93f8fdff9ae19e850e547c9b1c1
AbelPalominoRojas <abel.palominorojas@gmail.com>
2023-08-05 22:09:13 -0500

    Subcategory: Model Maduration Rest APIs Level 2

--- src/main/java/com/ironman/pharmasales/expose/web/SubcategoryController.java
+++ src/main/java/com/ironman/pharmasales/expose/web/SubcategoryController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,14 +26,16 @@ public class SubcategoryController {
     public ResponseEntity<List<SubcategoryDto>> findAll() {
         List<SubcategoryDto> subcategories = subcategoryService.findAll();
 
-        return ResponseEntity.ok(subcategories);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(subcategories);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<SubcategoryDto> findById(@PathVariable("id") Long id) throws DataNotFoundException {
         SubcategoryDto subcategory = subcategoryService.findById(id);
 
-        return ResponseEntity.ok(subcategory);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(subcategory);
     }
 
     @PostMapping
@@ -40,7 +43,8 @@ public class SubcategoryController {
             throws DataNotFoundException {
         SubcategoryDto subcategory = subcategoryService.create(subcategoryBody);
 
-        return ResponseEntity.ok(subcategory);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(subcategory);
     }
 
 
@@ -51,14 +55,16 @@ public class SubcategoryController {
     ) throws DataNotFoundException {
         SubcategoryDto subcategory = subcategoryService.edit(id, subcategoryBody);
 
-        return ResponseEntity.ok(subcategory);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(subcategory);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<SubcategoryDto> disabled(@PathVariable("id") Long id) throws DataNotFoundException {
         SubcategoryDto subcategory = subcategoryService.disabled(id);
 
-        return ResponseEntity.ok(subcategory);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(subcategory);
     }
 
     @GetMapping("/filter")
@@ -66,14 +72,16 @@ public class SubcategoryController {
 
         List<SubcategoryDto> subcategories = subcategoryService.filter(filter);
 
-        return ResponseEntity.ok(subcategories);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(subcategories);
     }
 
     @GetMapping("/pagination-filter")
     public ResponseEntity<Page<SubcategoryDto>> paginationFilter(Pageable pageable, Optional<SubcategoryFilterDto> filter) {
         Page<SubcategoryDto> subcategoryDtoPage = subcategoryService.paginationFilter(pageable, filter);
 
-        return ResponseEntity.ok(subcategoryDtoPage);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(subcategoryDtoPage);
     }
 
 }
